3cc107ca7be3680729f8326e36388e7f9748a7fa
Chinmay Vivek <chinmay.vivek@gmail.com>
2024-11-15 04:35:14 +0530

    start.sh added

---
+++ start.sh
@@ -0,0 +1,11 @@
+export SERVER_PORT=8080
+export PRODUCT_NAME=SwiftSchool
+export PRODUCT_VERSION=0.0.1
+
+export POSTGRES_HOST=localhost
+export DB_PORT=5432
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_DB=postgres
+
+go run main.go
