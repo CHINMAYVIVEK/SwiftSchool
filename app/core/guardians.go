78b2b7a808156ed2848d127f9a2b72ce65cbe7c6
Chinmay Vivek <chinmay.vivek@gmail.com>
2025-12-03 06:41:12 +0530

    guardians updated

--- app/core/guardians.go
+++ app/core/guardians.go
@@ -2,21 +2,31 @@ package core
 
 import (
 	"context"
-	"swiftschool/internal/db"
+	"swiftschool/domain"
+
+	"github.com/google/uuid"
 )
 
-func (s *Service) CreateGuardian(ctx context.Context, arg db.CreateGuardianParams) (db.CoreGuardian, error) {
-	createGuardian, err := s.repo.CreateGuardian(ctx, arg)
-	if err != nil {
-		return createGuardian, err
-	}
-	return createGuardian, nil
+//////////////////////////////////////////////////////
+//                 GUARDIAN METHODS                //
+//////////////////////////////////////////////////////
+
+// ========================= CREATE =========================
+func (s *Service) CreateGuardian(ctx context.Context, arg domain.Guardian) (*domain.Guardian, error) {
+	return s.repo.CreateGuardian(ctx, arg)
 }
 
-func (r *Repository) CreateGuardian(ctx context.Context, arg db.CreateGuardianParams) (db.CoreGuardian, error) {
-	createGuardian, err := r.CreateGuardian(ctx, arg)
-	if err != nil {
-		return createGuardian, err
-	}
-	return createGuardian, nil
+func (r *Repository) CreateGuardian(ctx context.Context, arg domain.Guardian) (*domain.Guardian, error) {
+	// TODO: implement DB logic here
+	return nil, nil
+}
+
+// ========================= LINK GUARDIAN =========================
+func (s *Service) LinkStudentGuardian(ctx context.Context, studentID, guardianID uuid.UUID) error {
+	return s.repo.LinkStudentGuardian(ctx, studentID, guardianID)
+}
+
+func (r *Repository) LinkStudentGuardian(ctx context.Context, studentID, guardianID uuid.UUID) error {
+	// TODO: implement DB logic here
+	return nil
 }
