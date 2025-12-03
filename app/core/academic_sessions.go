5641957d57297467f9048ecf33fdfe91ebc64455
Chinmay Vivek <chinmay.vivek@gmail.com>
2025-12-03 06:18:13 +0530

    academic_sessions updated

--- app/core/academic_sessions.go
+++ app/core/academic_sessions.go
@@ -3,7 +3,6 @@ package core
 import (
 	"context"
 	"swiftschool/domain"
-	"swiftschool/internal/db"
 
 	"github.com/google/uuid"
 )
@@ -17,21 +16,21 @@ func (s *Service) CreateAcademicSession(ctx context.Context, arg domain.Academic
 	return coreAcademicSession, nil
 }
 
-func (s *Service) ListAcademicSessions(ctx context.Context, instituteID uuid.UUID) ([]db.CoreAcademicSession, error) {
+func (s *Service) ListAcademicSessions(ctx context.Context, instituteID uuid.UUID) ([]*domain.AcademicSession, error) {
 	coreAcademicSessions, err := s.repo.ListAcademicSessions(ctx, instituteID)
 	if err != nil {
 		return coreAcademicSessions, err
 	}
 	return coreAcademicSessions, nil
 }
-func (s *Service) GetActiveSession(ctx context.Context, instituteID uuid.UUID) (db.CoreAcademicSession, error) {
+func (s *Service) GetActiveSession(ctx context.Context, instituteID uuid.UUID) (*domain.AcademicSession, error) {
 	coreAdmissionSessoin, err := s.repo.GetActiveSession(ctx, instituteID)
 	if err != nil {
 		return coreAdmissionSessoin, err
 	}
 	return coreAdmissionSessoin, nil
 }
-func (s *Service) UpdateAcademicSession(ctx context.Context, arg db.UpdateAcademicSessionParams) (db.CoreAcademicSession, error) {
+func (s *Service) UpdateAcademicSession(ctx context.Context, arg domain.AcademicSession) (*domain.AcademicSession, error) {
 	coreAcademicSession, err := s.repo.UpdateAcademicSession(ctx, arg)
 	if err != nil {
 		return coreAcademicSession, err
@@ -40,27 +39,17 @@ func (s *Service) UpdateAcademicSession(ctx context.Context, arg db.UpdateAcadem
 }
 
 func (r *Repository) CreateAcademicSession(context.Context, domain.AcademicSession) (*domain.AcademicSession, error) {
-
 	return nil, nil
 }
-func (r *Repository) ListAcademicSessions(ctx context.Context, instituteID uuid.UUID) ([]db.CoreAcademicSession, error) {
-	coreAcademicSessions, err := r.ListAcademicSessions(ctx, instituteID)
-	if err != nil {
-		return coreAcademicSessions, err
-	}
-	return coreAcademicSessions, nil
+
+func (r *Repository) ListAcademicSessions(ctx context.Context, instituteID uuid.UUID) ([]*domain.AcademicSession, error) {
+	return nil, nil
 }
-func (r *Repository) GetActiveSession(ctx context.Context, instituteID uuid.UUID) (db.CoreAcademicSession, error) {
-	coreAdmissionSessoin, err := r.GetActiveSession(ctx, instituteID)
-	if err != nil {
-		return coreAdmissionSessoin, err
-	}
-	return coreAdmissionSessoin, nil
+
+func (r *Repository) GetActiveSession(ctx context.Context, instituteID uuid.UUID) (*domain.AcademicSession, error) {
+	return nil, nil
 }
-func (r *Repository) UpdateAcademicSession(ctx context.Context, arg db.UpdateAcademicSessionParams) (db.CoreAcademicSession, error) {
-	coreAcademicSession, err := r.UpdateAcademicSession(ctx, arg)
-	if err != nil {
-		return coreAcademicSession, err
-	}
-	return coreAcademicSession, nil
+
+func (r *Repository) UpdateAcademicSession(ctx context.Context, arg domain.AcademicSession) (*domain.AcademicSession, error) {
+	return nil, nil
 }
